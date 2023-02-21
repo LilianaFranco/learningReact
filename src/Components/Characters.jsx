dbe5b4230d5d9498520f954802d166fdd70082af
LilianaFranco <franco0123@gmail.com>
2023-02-21 10:42:26 -0500

    Testing how Fragment works. I seems Tags should have the one and only father.

---
+++ src/Components/Characters.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Characters = () => {
+  return (
+    <React.Fragment>
+      <h1>Bob Esponja</h1>
+      <p>Patricio Star</p>
+    </React.Fragment>
+  );
+};
+
+//React solo permite que cada componente retorne un solo elemento JSX como “padre”.

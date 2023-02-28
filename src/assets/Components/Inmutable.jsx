40810bb886171c50edc1f46c0a2e9ee870078975
LilianaFranco <franco0123@gmail.com>
2023-02-28 10:24:17 -0500

    Use useState to change states inside a component. Two buttons to add and substract.

--- src/assets/Components/Inmutable.jsx
+++ src/assets/Components/Inmutable.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 export const Inmutable = () => {
-  let iteration = 0;
-  const sum = () => {
-    console.log("Hi");
+  const [iteration, setIeteration] = useState(5);
+
+  const add = () => {
+    setIeteration(iteration + 1);
   };
+
+  const substract = () => {
+    setIeteration(iteration - 1);
+  };
+
+  // let iteration = 5; Components are inmutable, therefore i can't change this variable inside the component.
+
   return (
     <div>
       <h1>Soy un componente inmutable.</h1>
-      <button onClick={sum}>Sumar</button>
+      <h2>{iteration}</h2>
+      <button onClick={add}>Sumar</button>
+      <button onClick={substract}>Restar</button>
     </div>
   );
 };
